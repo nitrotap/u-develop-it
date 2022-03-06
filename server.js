9479bb6ca76cb1d2ee77ca3de36fd4ef3d93e04f
nitrotap <kartikinpublic@gmail.com>
2022-03-05 22:44:22 -0700

    connecting to sql database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'SQLr0cks!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
